860544c254d851f068b0eb8f79a926987d5af6d2
Stefano Casciarri <s18449@pjwstk.edu.pl>
2021-02-05 20:46:05 +0100

    Cwiczenie 4

--- cw3/Controllers/StudentsController.cs
+++ cw3/Controllers/StudentsController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,11 +12,64 @@ namespace cw3.Controllers
     [Route("api/students")]
     public class StudentsController : ControllerBase
     {
+
+        private const string ConString = "Data Source=db-mssql;Initial Catalog=s18449;Integrated Security=True";
+
         [HttpGet]
-        public string GetStudent(string orderBy)
+        public IActionResult GetStudent()
         {
-            return $"Kowalski, Malewski, Andrzejewski sortowanie={orderBy}";
+            var list = new List<Student>();
+
+            using (SqlConnection con = new SqlConnection(ConString)) 
+            using (SqlCommand com = new SqlCommand())
+            {
+                com.Connection = con;
+                com.CommandText = "select * from students";
+
+                con.Open();
+                SqlDataReader dr = com.ExecuteReader();
+
+                while(dr.Read())
+                {
+                    var st = new Student();
+                    st.IndexNumber = dr["IndexNumber"].ToString();
+                    st.FirstName = dr["FirstName"].ToString();
+                    st.LastName = dr["LastName"].ToString();
+                }
+
+                return Ok(list);
+
+            }
+
         }
+
+        [HttpGet("{idStudenta}")]
+        public IActionResult GetStudent(string idStudenta)
+        {
+
+            using (SqlConnection con = new SqlConnection(ConString))
+            using (SqlCommand com = new SqlCommand())
+            {
+                com.Connection = con;
+                com.CommandText = "select semestr from students where idstudy=@id";
+
+                com.Parameters.AddWithValue("id", idStudenta);
+
+                con.Open();
+                SqlDataReader dr = com.ExecuteReader();
+
+                if (dr.Read())
+                {
+                    string Semestr = dr["Semester"].ToString();
+                    
+                    return Ok(Semestr);
+                }
+
+            }
+
+            return NotFound();
+        }
+
 
         [HttpPost]
         public IActionResult CreateStudent(Student student)
