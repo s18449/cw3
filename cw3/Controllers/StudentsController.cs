6b2e4722d6bc7ee04e71ef3c2a57fb172e44df85
Stefano Casciarri <s18449@pjwstk.edu.pl>
2021-02-05 19:26:37 +0100

    Dodaj pliki projektów.

---
+++ cw3/Controllers/StudentsController.cs
@@ -0,0 +1,42 @@
+﻿using cw3.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cw3.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetStudent(string orderBy)
+        {
+            return $"Kowalski, Malewski, Andrzejewski sortowanie={orderBy}";
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            //...add to database
+            //...generating index number
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            return Ok(student);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateStudent(string studentId)
+        {
+            return Ok("Aktualizacja studenta o id " + studentId + " dokończona");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteStudent(string studentId)
+        {
+            return Ok("Usuwanie studenta o id " + studentId + " ukończone");
+        }
+
+    }
+}
